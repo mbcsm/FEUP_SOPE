ef889869b03fc3068e6aad26381ded420d6aa421
mbcsm <manelbcsm1@gmail.com>
2018-04-30 12:41:23 +0100

    create threads

--- proj2/server.c
+++ proj2/server.c
@@ -8,20 +8,47 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <pthread.h>
 #include "header.h"
 
+int num_room_seats,
+		num_ticket_offices,
+		open_time;
+	
+int *createThreadForRequest(){
+
+  unsigned long i = 0;
+  pthread_t id = pthread_self();
+
+
+  return 0;
+}
 
 int receiveClientRequests(){
+	pthread_t tid[num_ticket_offices];
+	
 	int fd, nread;
-  const char *fifo_name = "/requests";
+  const char *fifo_name = "requests";
   char buf[256] ;
   fd = open(fifo_name, O_RDONLY);
   
   int iteration = 0;
 	while(1) {
+	
     memset(buf, 0, 256);
-    nread = read(fd, buf, 256);
-    printf("%d - %s\n", iteration, buf);
+    
+    if(read(fd, buf, 256)>0){
+   		printf("%d - %s\n", iteration, buf);
+		  int i = 0;
+			int err;
+
+			err = pthread_create(&(tid[i]), NULL, createThreadForRequest, NULL);
+			if (err != 0)
+				printf("\ncan't create thread :[%s]", strerror(err));
+			else
+				printf("\n Thread created successfully\n");
+    }
+		
     iteration++;
     sleep(1);
 	}
@@ -34,11 +61,11 @@ int main(int argc, char* argv[]) {
     return -1;
   }
   
-	char* num_room_seats = argv[1];
-	char* num_ticket_offices = argv[2];
-	char* open_time = argv[3];
+	num_room_seats = atoi(argv[1]);
+	num_ticket_offices = atoi(argv[2]);
+	open_time = atoi(argv[3]);
 	
-	
+        
 	receiveClientRequests();
 
 	
