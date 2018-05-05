93b0bf4e4548b752c998cd50ecd308b18b7cce3b
mbcsm <manelbcsm1@gmail.com>
2018-05-05 10:42:05 +0100

    Update server.c
    threads are only created once. start work on the mutex variables

--- proj2/server.c
+++ proj2/server.c
@@ -10,23 +10,64 @@
 #include <fcntl.h>
 #include <pthread.h>
 #include "header.h"
-
+#include <semaphore.h>
+	
+	
+volatile int* array[10000];
+pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+  
 int num_room_seats,
 		num_ticket_offices,
 		open_time;
 	
-int *createThreadForRequest(){
+	
+struct Seats {
+  int seat;
+  int mtx;
+} seat;
 
+struct Requests {
+  char requestInfo[256] ;
+} request;
+
+
+void *createThreadForTicketBooth(void *pointer){
   unsigned long i = 0;
   pthread_t id = pthread_self();
+  
+  pthread_mutex_lock(&mtx);
+  
+	pthread_mutex_unlock(&mtx);
 
-
-  return 0;
+  return NULL;
 }
 
-int receiveClientRequests(){
+
+void * receiveClientRequests(void * arg) {
+	
+  struct Seats seat[num_room_seats];
+  struct Requests request;
+  
+  //opening booths threads
 	pthread_t tid[num_ticket_offices];
 	
+	printf("\n Openning [%d] Booths\n", num_ticket_offices);
+	for(int i = 0; i < num_ticket_offices; i++){
+		  int i = 0;
+			int err;
+			err = pthread_create(&(tid[i]), NULL, createThreadForTicketBooth, NULL);
+			if (err != 0){
+				printf("\ncan't create thread :[%s]", strerror(err));
+				return -1;
+			}
+			else{
+				printf("\n Booth Openned successfully\n");
+    		pthread_join(tid[i], NULL); 
+			}
+	}
+	
+	
+	//reading from FIFO
 	int fd, nread;
   const char *fifo_name = "requests";
   char buf[256] ;
@@ -39,14 +80,7 @@ int receiveClientRequests(){
     
     if(read(fd, buf, 256)>0){
    		printf("%d - %s\n", iteration, buf);
-		  int i = 0;
-			int err;
-
-			err = pthread_create(&(tid[i]), NULL, createThreadForRequest, NULL);
-			if (err != 0)
-				printf("\ncan't create thread :[%s]", strerror(err));
-			else
-				printf("\n Thread created successfully\n");
+   		
     }
 		
     iteration++;
@@ -54,8 +88,8 @@ int receiveClientRequests(){
 	}
 }
 
+
 int main(int argc, char* argv[]) {
-	printf("%d\n", argc);
   if(argc != 4){
     printf("ERROR: wrong number of arguments\n Try [options] <pattern> <filepath>\n");
     return -1;
@@ -65,8 +99,21 @@ int main(int argc, char* argv[]) {
 	num_ticket_offices = atoi(argv[2]);
 	open_time = atoi(argv[3]);
 	
-        
-	receiveClientRequests();
+	
+	
+	printf("\n Openning Store\n");
+	pthread_t mainThreadId;
+  int err;
+	err = pthread_create(&mainThreadId, NULL, receiveClientRequests, NULL);
+	if (err != 0){
+		printf("\ncan't create thread :[%s]", strerror(err));
+		return -1;
+	}
+	else{
+		printf("\n Store Opened\n");
+		pthread_join(mainThreadId, NULL); 
+	}   
+	  
 
 	
   return 0;
